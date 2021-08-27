390a2e7dc542551cd9ed83fafedacbe264c5572f
Armynerh <aimenah@outlook.com>
2021-08-27 17:54:07 +0100

    Print more alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,30 +1,22 @@
 #include <stdio.h>
+/* more headers goes there */
+/**
+ * main - main block
+ * Use the function puts
+ * You are not allowed to use printf
+ * Description: Use `putchar` to print all letters but the letter 'q' and 'e'.
+ * Return: 0
+ */
+/* betty style doc for function main goes there */
 int main(void)
 {
-	int i, j, k;
+	char c;
 
-	i = 0;
-
-	while (i < 100)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		j = i % 10; /* singles digit */
-		k = i / 10; /* doubles digit */
-
-		if (k < j)
-		{
-			putchar(k + '0');
-			putchar(j + '0');
-
-			if (i < 89)
-			{
-				putchar(44);
-				putchar(32);
-			}
-		}
-
-		i++;
+		if (c != 'e' && c != 'q')
+			putchar(c);
 	}
 	putchar('\n');
-
 	return (0);
 }
