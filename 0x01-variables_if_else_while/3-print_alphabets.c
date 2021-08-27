54a03b9414527e033db69ebbd7efb50f9363e59a
Armynerh <aimenah@outlook.com>
2021-08-27 17:58:27 +0100

    Print more alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,19 +4,18 @@
  * main - main block
  * Use the function puts
  * You are not allowed to use printf
- * Description: Use `putchar` to print all letters but the letter 'q' and 'e'.
+ * Description: Use `putchar` to print lowercase and then uppercase alphabet.
  * Return: 0
  */
 /* betty style doc for function main goes there */
 int main(void)
 {
-	char c;
+	char c, d;
 
 	for (c = 'a'; c <= 'z'; c++)
-	{
-		if (c != 'e' && c != 'q')
-			putchar(c);
-	}
+		putchar(c);
+	for (d = 'A'; d <= 'Z'; d++)
+		putchar(d);
 	putchar('\n');
 	return (0);
 }
