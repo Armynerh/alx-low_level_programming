cf1e7c387102ce138356952b6e9e7b5add9ea4ee
Armynerh <aimenah@outlook.com>
2021-08-27 18:31:02 +0100

    Quote the Last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,12 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 /**
- * main - main block
- * Return: 1
+ * main - Print string to standard error
+ *
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-	int s = strlen(""and that piece of art is useful - Dora Korpar, 2015-10-19\n"");
-
-	write(2,
-	      ""and that piece of art is useful - Dora Korpar, 2015-10-19\n"",
-	      s);
+	write(2, "and that piece of art is useful\" - Dora Korpar, \
+2015-10-19\n", 59);
 	return (1);
 }
