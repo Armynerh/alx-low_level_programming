0c5d9e8e3226e3373d886860104fcc618f524ce0
Armynerh <aimenah@outlook.com>
2021-08-31 21:26:02 +0100

    There are no absolutes

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * _abs - function that prints the absolute value of a number
+  * @n: takes a int
+  * Return: the abs
+  */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}
