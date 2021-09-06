c3b428cb34b0b24e592da94ef6a9d6448d58e0de
Armynerh <aimenah@outlook.com>
2021-09-06 21:03:08 +0100

    Task

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,16 @@
 #include "main.h"
-
 /**
- * _puts - print a string to stdout
- * @str: char array string type
- * Description: Can only use _putchar
+ * _puts - a function that prints a string
+ * @str: string input
+ * Return: string
  */
-
 void _puts(char *str)
 {
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+		_puts(&str[i]);
 	}
-	_putchar('\n');
+	_puts("\n");
+}
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * print_rev - Write a function that prints a string, in reverse.
- * @s: take an pointer
+ * print_rev - print a string in reverse
+ * @s: char array string type
  */
 void print_rev(char *s)
 {
-	int i = 0, c;
+	int i;
 
-	while (s[c] != '\0')
-		c++;
-	c--;
-	for (i = c; i >= 0; i--)
-		_putchar(s[i]);
-	_putchar('\n');
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	for (i--; i >= 0; i--)
+		print_rev(&s[i]);
+
+	print_rev("\n");
 }
-
