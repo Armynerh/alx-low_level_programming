c4ea7124a405565dd235f16c7cb13f7a18b2929e
Armynerh <aimenah@outlook.com>
2021-09-15 19:08:17 +0100

    Tasks

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,17 @@
-#inclide "main.h"
+#include "main.h"
 
 /**
-* _puts_recursion - prints a string, followed by a new line
-* @s: pointr to memory area
-* Return: Always 0
-*/
+ * _puts_recursion - Prints a string with recursion
+ * @s: Pointer to memory area
+ */
 
-void _puts_recursion(char *s);	
+void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		putchar(*s);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else
-	{	
-		putchar('\n');
-	}
+		_putchar('\n');
 }
