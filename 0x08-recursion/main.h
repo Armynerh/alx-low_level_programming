9c513767fc9bac8085c0ef52dde73c125cfde55a
Armynerh <aimenah@outlook.com>
2021-09-15 22:18:02 +0100

    Task

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,8 +5,12 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int is_palindrome(char *s);
+int halp(int c, int i);
 int is_prime_number(int n);
-
-#include <stdio.h>
-
+int hai(int n, int d);
+int _sqrt(int x, int i);
+int is_palindrome(char *s);
+int get_length(char *s);
+int my_pal(char *s, int l);
+int wildcmp(char *s1, char *s2);
+int findsrc(char *s, char c, int i, int p);
