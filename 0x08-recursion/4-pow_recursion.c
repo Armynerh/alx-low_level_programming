7b3055d56126af6b9ca2ce3147448af6ab80d1a0
Armynerh <aimenah@outlook.com>
2021-09-16 01:30:53 +0100

    Task

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * Power- 
- * @x:
- * @y:
- * Return
+ * _pow_recursion - returns the power of a variable 
+ * @x: base
+ * @y: power
+ * return: int
  */
 
 int _pow_recursion(int x, int y)
@@ -14,7 +14,11 @@ int _pow_recursion(int x, int y)
 		return (1);
 	}
 	else if ( y ==  0)
+	{
 		return (-1);
-
-	return (x * _pow_recursion (x, y - 1));
+	}
+	else
+	{
+		return (x * _pow_recursion (x, y - 1));
+	}
 }
