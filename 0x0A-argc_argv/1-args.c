88758274afdf92cd408b567d749cbbebfc97811e
Armynerh <aimenah@outlook.com>
2021-09-18 18:43:56 +0100

    Task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* program that prints the number of arguments passed into it
-*@argc: prints number of arguments
-*@argv: prints strings
-*Return: Always 0
-*/ 
-
+ * main - prints the number of arguments.
+ * @argc: argument count
+ * @argv: Argument vector for values
+ *
+ *
+ * Return: Always to 0
+ */ 
 int main(int argc, char* argv[])
 {
 	(void)argv;
