ae74885dfac817aa4e9de236837b2eefb1928e2e
Armynerh <aimenah@outlook.com>
2021-09-23 23:14:25 +0100

    Task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,56 +1,62 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * _strlen - string length
- * @str: string
- * Return: length string
- * *string_nconcat - string concatenation
- * @s1: char 1
- * @s2: char 2
- * @n: number
- * Return: new string
+ * _strlen - give the length of a string
+ * @s: the string
+ *
+ * Return: the length of a string
  */
-unsigned int _strlen(char *str)
+unsigned int _strlen(char *s)
 {
-	unsigned int con = 0;
+	unsigned int i;
 
-	while (str[con] != 0)
-		con++;
-	return (con);
+	for (i = 0 ; s[i] != '\0' ; i++)
+		;
+	return (i);
 }
 
-
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes of s2
+ *
+ * Return: a pointer with the content of s1 followed by n byte of s2
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *newstrn;
-	unsigned int strlen_s1;
-	unsigned int strlen_s2;
-	unsigned int _malloc, i;
+	unsigned int length1, length2, i, j;
+	char *space;
 
 	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	strlen_s1 = _strlen(s1);
-	strlen_s2 = _strlen(s2);
-
-	if (n >= strlen_s2)
-		_malloc = strlen_s1 + strlen_s2;
-	else
-		_malloc = strlen_s1 + n;
-
-	newstrn = malloc(sizeof(char) * (_malloc + 1));
-	if (newstrn == NULL)
-		return (NULL);
-	for (i = 0; i < _malloc; i++)
 	{
-		if (i < strlen_s1)
-			newstrn[i] = s1[i];
-		else
-			newstrn[i] = s2[i - strlen_s1];
+		s1 = "";
 	}
-	newstrn[i] = '\0';
-	return (newstrn);
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	length1 = _strlen(s1);
+	length2 = _strlen(s2);
+
+	if (n >= length2)
+	{
+		n = length2;
+	}
+
+	space = malloc(sizeof(char) * (n + length1 + 1));
+
+	if (space == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0 ; i < length1 ; i++)
+		space[i] = s1[i];
+	for (j = 0 ; s2[j] != '\0' && j != n ; j++, i++)
+		space[i] = s2[j];
+
+	space[i] = '\0';
+	return (space);
 }
