ab6d0e7869f058bb40141cf8a484d2136349620d
Armynerh <aimenah@outlook.com>
2021-09-23 22:23:03 +0100

    Task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,62 +1,51 @@
 #include "main.h"
-#include <stdlib.h>
-/**
- * _strlen - give the length of a string
- * @s: the string
- *
- * Return: the length of a string
- */
-unsigned int _strlen(char *s)
-{
-	unsigned int i;
+#include <stdio.h>
 
-	for (i = 0 ; s[i] != '\0' ; i++)
-		;
-	return (i);
+/**
+ * string_nconcat - concatenate 2 strings, only n bytes of s2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes to include of s2
+ * Return: NULL if fail, else pointer to malloc memory
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	int strlen1, i, c;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	strlen1 = (unsigned int)_strlen(s1);
+	p = malloc((strlen1 + n + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0, c = 0; i < (strlen1 + n); i++)
+	{
+		if (i < strlen1)
+			p[i] = s1[i];
+		else
+			p[i] = s2[c++];
+	}
+	p[i] = '\0';
+
+	return (p);
 }
 
 /**
- * string_nconcat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * @n: bytes of s2
- *
- * Return: a pointer with the content of s1 followed by n byte of s2
+ * _strlen - find length of string
+ * @s: string
+ * Return: length of string
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+
+int _strlen(char *s)
 {
-	unsigned int length1, length2, i, j;
-	char *space;
+	int i;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-
-	length1 = _strlen(s1);
-	length2 = _strlen(s2);
-
-	if (n >= length2)
-	{
-		n = length2;
-	}
-
-	space = malloc(sizeof(char) * (n + length1 + 1));
-
-	if (space == NULL)
-	{
-		return (NULL);
-	}
-
-	for (i = 0 ; i < length1 ; i++)
-		space[i] = s1[i];
-	for (j = 0 ; s2[j] != '\0' && j != n ; j++, i++)
-		space[i] = s2[j];
-
-	space[i] = '\0';
-	return (space);
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
 }
