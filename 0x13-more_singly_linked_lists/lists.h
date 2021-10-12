443ea624b8e910c1f41cf8d3fb1d541ea42b9faa
Armynerh <aimenah@outlook.com>
2021-10-12 11:23:41 +0100

    Tasks

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,12 @@
-#ifndef _LISTS_H
-#define _LISTS_H
+
+   
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -15,6 +20,9 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+/* Prototypes  */
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -25,9 +33,7 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
-size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-#endif
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+#endif /* LISTS_H */
